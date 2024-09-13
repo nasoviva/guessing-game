00928df0369ebd75b8b773e3bf07fc70579c553c
nasoviva <nasoviva@gmail.com>
2024-09-13 14:08:26 -0300

    feat:implementing is ready

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.res = Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        if (this.min < this.max) {
+            this.max = this.res;
+        }
     }
 
     greater() {
-
+        if (this.min < this.max) {
+            this.min = this.res;
+        }
     }
 }
 
